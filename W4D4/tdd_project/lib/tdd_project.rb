7c6147ab75bac06ea892c7ca5eca70a6793ff409
Ezekiel Yu <cellozeke@gmail.com>
2021-06-03 15:30:36 -0700

    did two_sum

--- W4D4/tdd_project/lib/tdd_project.rb
+++ W4D4/tdd_project/lib/tdd_project.rb
@@ -1,11 +1,22 @@
-class Array     
+class Array
     def my_uniq
         new_arr = []
 
-        each {|el| new_arr << el unless new_arr.include?(el) }
+        each { |el| new_arr << el unless new_arr.include?(el) }
 
-        new_arr 
+        new_arr
 
         # uniq
-    end 
+    end
+
+    def two_sum
+        pairs = []
+        (0...length - 1).each do |i|
+            (i + 1...length).each do |j|
+                pairs << [i, j] if self[i] == -self[j]
+            end
+        end
+        pairs
+        # [[2, 3], [0, 4]]
+    end
 end
--- W4D4/tdd_project/spec/tdd_project_spec.rb
+++ W4D4/tdd_project/spec/tdd_project_spec.rb
@@ -2,7 +2,7 @@ require 'tdd_project'
 
 describe "#my_uniq" do 
     subject(:arr) { [1,1,3,4,2,3] }
-    context "an array of elements" do 
+    context "given an array of elements" do 
         it "returns the array without duplicates" do 
             expect(arr.my_uniq).to eq [1,3,4,2] 
         end
@@ -13,3 +13,14 @@ describe "#my_uniq" do
     end 
 end 
 
+describe '#two_sum' do
+    subject(:arr) { [-1, 0, 2, -2, 1] }
+    context 'when an array of integers is given' do
+        it 'returns all pairs of positions where the elements sum to zero' do
+            expect(arr.two_sum.sort).to eq [[0, 4], [2, 3]]
+        end
+        it 'pairs should be sorted "dictionary-wise"' do
+            expect(arr.two_sum).to eq [[0, 4], [2, 3]]
+        end
+    end
+end
