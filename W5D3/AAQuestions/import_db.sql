e9604960deb7fbe4b9199a04fa5e0d02256af64f
Joseph Kim <jkjoeeekim@live.com>
2021-06-09 12:25:10 -0700

    Add users class

--- W5D3/AAQuestions/import_db.sql
+++ W5D3/AAQuestions/import_db.sql
@@ -1,10 +1,10 @@
 PRAGMA foreign_keys = ON;
 
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS questions;
-DROP TABLE IF EXISTS question_follows;
-DROP TABLE IF EXISTS replies;
 DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
@@ -44,7 +44,6 @@ CREATE TABLE replies (
 
 CREATE TABLE question_likes (
   id INTEGER PRIMARY KEY,
-  likes TEXT,
   user_id INTEGER NOT NULL,
   question_id INTEGER NOT NULL,
 
@@ -83,14 +82,10 @@ VALUES
   ('This is the last reply', 2, 2, 1);
 
 INSERT INTO
-  question_likes (likes, user_id, question_id)
+  question_likes (user_id, question_id)
 VALUES
-  ('FALSE', 1, 1),
-  ('TRUE', 1, 2),
-  ('TRUE', 1, 3),
-  ('FALSE', 2, 1),
-  (NULL, 2, 2),
-  ('TRUE', 2, 3),
-  (NULL, 3, 1),
-  ('FALSE', 3, 2),
-  ('TRUE', 3, 3);
+  (1, 1),
+  (2, 1),
+  (2, 3),
+  (3, 2),
+  (3, 3);
---
+++ W5D3/AAQuestions/questions_database.rb
@@ -0,0 +1,63 @@
+require 'singleton'
+require 'sqlite3'
+
+class QuestionsDatabase < SQLite3::Database
+  include Singleton
+
+  def initialize
+    super('questions.db')
+    self.type_translation = true
+    self.results_as_hash = true
+  end
+end
+
+class Users
+  def self.find_by_id(id)
+    user = QuestionsDatabase.instance.execute(<<-SQL, id)
+      SELECT *
+      FROM users
+      WHERE id = ?
+    SQL
+    return nil unless user.length.positive?
+
+    Users.new(user.first)
+  end
+
+  def self.find_by_name(fname, lname)
+    users = QuestionsDatabase.instance.execute(<<-SQL, fname, lname)
+      SELECT *
+      FROM users
+      WHERE fname = ? AND lname = ?
+    SQL
+    return nil unless users.length.positive?
+
+    users.map! { |user| Users.new(user) }
+  end
+
+  attr_reader :id
+  attr_accessor :fname, :lname
+
+  def initialize(options)
+    @id = options['id']
+    @fname = options['fname']
+    @lname = options['lname']
+  end
+
+
+end
+
+class Questions
+
+end
+
+class QuestionFollows
+
+end
+
+class Replies
+
+end
+
+class QuestionLikes
+
+end
