c5de9ddd2714b01b1fcf32684554d86dd6648c8b
Jenny Nhan <j3nnynhan@gmail.com>
2021-06-11 15:03:45 -0700

    last two left

--- W5D5/skeleton/movie_buff/01_queries.rb
+++ W5D5/skeleton/movie_buff/01_queries.rb
@@ -7,6 +7,7 @@ def it_was_ok
   #
   # Find the id, title, and score of all movies with scores between 2 and 3
 
+  Movie.select(:id, :title, :score).where(score: 2..3)
 end
 
 def harrison_ford
@@ -20,6 +21,12 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
+
+  Movie
+    .select(:id, :title)
+    .joins(:actors)
+    .where(actors: {name: 'Harrison Ford'})
+    .where.not(castings: {ord: 1})
 
 end
 
@@ -38,6 +45,13 @@ def biggest_cast
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
 
+  Movie
+    .select(:id, :title)
+    .joins(:castings)
+    .group(:id)
+    .order('COUNT(*) DESC')
+    .limit(3)
+
 end
 
 def directed_by_one_of(them)
@@ -52,7 +66,7 @@ def directed_by_one_of(them)
   # Movie.where(yr: years)
   #
   # Find the id and title of all the movies directed by one of 'them'.
-
+  
 end
 
 def movie_names_before_1940
