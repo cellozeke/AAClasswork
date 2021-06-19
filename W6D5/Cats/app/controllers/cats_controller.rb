bf060beeb1ced7ac571e8e49962629d042aabe33
Ezekiel Yu <cellozeke@gmail.com>
2021-06-18 17:02:34 -0700

    W6D5 stuck on authenticity

--- W6D5/Cats/app/controllers/cats_controller.rb
+++ W6D5/Cats/app/controllers/cats_controller.rb
@@ -14,4 +14,20 @@ class CatsController < ApplicationController
       @cat = Cat.new
       render :new
     end
+
+    def create
+        @cat = Cat.new(cat_params)
+        if @cat.save
+        # redirect_to cat_url(@cat.id)
+        # redirect_to "localhost:3000/cats/#{@cat.id}"
+        redirect_to cat_url(@cat)
+        else
+        # render json: @cat.errors.full_messages, status: 422
+        render :new
+        end
+    end
+
+    def cat_params
+        params.require(:cat).permit(:birth_date, :color, :name, :sex, :description)
+    end
 end
