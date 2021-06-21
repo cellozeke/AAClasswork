68b248b9bcdd927358babd8b52fc991b6917db39
b-chai <b.chai82@gmail.com>
2021-06-21 17:34:30 -0400

    finished users

--- W7D1/skeleton/app/models/user.rb
+++ W7D1/skeleton/app/models/user.rb
@@ -1,9 +1,51 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  user_name       :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
   after_initialize :ensure_session_token
   validates :user_name, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: true
 
+  def self.find_by_credentials(user_name, password)
+    user = User.find_by(user_name: user_name)
+    if user && user.check_password?(password)
+      user
+    else
+      nil
+    end
+  end
+
   def ensure_session_token
     self.session_token ||= SecureRandom::urlsafe_base64
   end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  # def password
+  #   @password
+  # end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def check_password?(password)
+    password_object = BCrypt::Password.new(self.password_digest)
+    password_object.is_password?(password)
+  end
+
+
 end
