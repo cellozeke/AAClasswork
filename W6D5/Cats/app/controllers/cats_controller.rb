c2b668f3da9c51bdeb0c52c55a09f9fc2bcb20bd
Ezekiel Yu <cellozeke@gmail.com>
2021-06-20 23:00:51 -0700

    finished Cats phase I

--- W6D5/Cats/app/controllers/cats_controller.rb
+++ W6D5/Cats/app/controllers/cats_controller.rb
@@ -17,13 +17,24 @@ class CatsController < ApplicationController
 
     def create
         @cat = Cat.new(cat_params)
-        if @cat.save
-        # redirect_to cat_url(@cat.id)
-        # redirect_to "localhost:3000/cats/#{@cat.id}"
-        redirect_to cat_url(@cat)
+        if @cat.save!
+            redirect_to cat_url(@cat)
         else
-        # render json: @cat.errors.full_messages, status: 422
-        render :new
+            render :new
+        end
+    end
+
+    def edit
+        @cat = Cat.find(params[:id])
+        render :edit
+    end
+
+    def update
+        @cat = Cat.find(params[:id])
+        if @cat.update(cat_params)
+          redirect_to cat_url(@cat)
+        else
+          render :edit
         end
     end
 
