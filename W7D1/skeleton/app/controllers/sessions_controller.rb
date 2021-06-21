5225ec321dfd584e39bb6b97d3d6e5298543cf97
b-chai <b.chai82@gmail.com>
2021-06-21 19:32:59 -0400

    things work now

--- W7D1/skeleton/app/controllers/sessions_controller.rb
+++ W7D1/skeleton/app/controllers/sessions_controller.rb
@@ -4,10 +4,19 @@ class SessionsController < ApplicationController
     render :new
   end
 
-  # def create
+  def create
+    @user = User.find_by_credentials(params[:user][:user_name],params[:user][:password])
 
-  # end
+    if @user
+      login(@user)
+      redirect_to cats_url
+    else
+      render :new
+    end
+  end
 
   def destroy
+    logout!
+    redirect_to new_session_url
   end
 end
