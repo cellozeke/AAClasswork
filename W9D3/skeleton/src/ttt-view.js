6f520fe0ef886c96157a72897e23f14268da8488
Samuel Song <samsongs@umich.edu>
2021-07-07 18:34:21 -0600

    Completed TicTacToe vanilla javascript project

--- W9D3/skeleton/src/ttt-view.js
+++ W9D3/skeleton/src/ttt-view.js
@@ -25,17 +25,45 @@ class View {
 
   handleClick(e) {
     let pos = e.target.getAttribute('data-pos');
-    // let pos = JSON.parse(e.target.getAttribute('data-pos'));
     console.log(this);
     console.log(this.game);
-    this.makeMove(pos);
+    try {
+      this.makeMove(pos); 
+    } catch(MoveError) {
+      alert("Bad move!");
+    }
   };
 
   makeMove(square) {
     const li = document.querySelector("li[data-pos="+`'${square}'`+"]"); // MAY NEED TO FIX
-    console.log(li);
-    li.setAttribute('data-mark', this.game.currentPlayer);
+    const current = this.game.currentPlayer;
+    
     this.game.playMove(JSON.parse(square));
+    li.setAttribute('data-mark', current);
+    
+    let gameOver = this.game.isOver();
+    if(gameOver) {
+      let fig = document.querySelector(".ttt");
+      let label = null;
+      let img = null;
+      if(this.game.winner() === 'x') {
+        label = document.createElement("label");
+        label.innerHTML = "Congrats RUBY!!!";
+        img = document.createElement("img");
+        img.setAttribute("src", "../dist/ruby.png");
+        fig.appendChild(img);
+      } else if(this.game.winner() === 'o') {
+        label = document.createElement("label");
+        label.innerHTML = "BOOOOOOOOO JAVASCRIPT!!!";
+        img = document.createElement("img");
+        img.setAttribute("src", "../dist/js.png");
+        fig.appendChild(img);
+      } else {
+        label = document.createElement("label");
+        label.innerHTML = "Tie: nobody wins";
+      }
+      fig.appendChild(label);
+    }
   }
 }
 
