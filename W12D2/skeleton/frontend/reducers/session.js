6e5beb6ce8a53cdaa1d0ac970a0b49b41d26cd7e
Ezekiel Yu <cellozeke@gmail.com>
2021-07-27 15:36:50 -0700

    W12D2 halfway through frontend auth project

---
+++ W12D2/skeleton/frontend/reducers/session.js
@@ -0,0 +1,19 @@
+import { LOGOUT_CURRENT_USER, RECEIVE_CURRENT_USER } from "../actions/session";
+
+const _nullSession = {
+  currentUser: null
+}
+
+const sessionReducer = (state=_nullSession, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return { currentUser: action.user };
+    case LOGOUT_CURRENT_USER:
+      return _nullSession;
+    default:
+      return state;
+  };
+}
+
+export default sessionReducer;
